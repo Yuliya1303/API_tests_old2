65cc04362b7ef5788f29cc1b6e59549a703e0d22
Yuliya Byshko <Yuliyalevkovets1303@gmail.com>
2022-06-09 22:14:47 +0200

    Initial commit

---
+++ src/test/java/com/yuliya1303/config/App.java
@@ -0,0 +1,7 @@
+package com.yuliya1303.config;
+
+import org.aeonbits.owner.ConfigFactory;
+
+public class App {
+    public static AppConfig config = ConfigFactory.create(AppConfig.class, System.getProperties());
+}
---
+++ src/test/java/com/yuliya1303/tests/DemowebshopTests.java
@@ -0,0 +1,51 @@
+package com.yuliya1303.tests;
+
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.yuliya1303.helpers.AllureRestAssuredFilter.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+
+public class DemowebshopTests extends TestBase {
+
+    static String login = "qaguru@qa.guru",
+            password = "qaguru@qa.guru1",
+            authCookieName = "NOPCOMMERCE.AUTH";
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndCustomListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+}
